428c0707b7db8bdb6d9a4aae1e62d2f408fc62fc
kathytrinh81 <kathytrinh81@yahoo.com>
2015-01-26 20:05:43 -0700

    Echo assignment
    Echo is done.

--- js/echo.js
+++ js/echo.js
@@ -1,8 +1,16 @@
-function echo()
-{
-  // ...
-}
+    function echo() {
+      
+      var args = Array.prototype.slice.call(arguments);
+      if (typeof args[0] === 'undefined') {
+            document.write("<br/>");
+          } else{
+      		args.forEach(function(arg) {
+           
+            document.write(arg + "<br/>");
+          
+      });}
+      }
 
-echo();
-echo('bla');
-echo('foo', 'bar', 'baz');
+      echo();
+      echo('bla');
+      echo('foo', 'bar', 'baz');
